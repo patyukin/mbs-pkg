474f3b562f87dda3dea0fa56d563d694fb6f776c
Pavel Tyukin <me@patyukin.ru>
2024-11-04 13:08:45 +0300

    update AuthSignInCode

--- pkg/rabbitmq/model.go
+++ pkg/rabbitmq/model.go
@@ -6,6 +6,7 @@ import (
 
 type AuthSignInCode struct {
 	Code       string    `json:"code"`
+	ChatID     int64     `json:"chat_id"`
 	UserUUID   string    `json:"user_uuid"`
 	Expiration time.Time `json:"expiration"`
 }
