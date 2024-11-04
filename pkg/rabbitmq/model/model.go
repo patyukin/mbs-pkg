ffa11c8e23106146d6fdb1d8098d4bb3744bfcee
Pavel Tyukin <me@patyukin.ru>
2024-11-04 19:13:11 +0300

    add AuthSignUpConfirmCode

--- pkg/rabbitmq/model/model.go
+++ pkg/rabbitmq/model/model.go
@@ -4,3 +4,10 @@ type AuthSignInCode struct {
 	Code   string `json:"code"`
 	ChatID int64  `json:"chat_id"`
 }
+
+type AuthSignUpConfirmCode struct {
+	Code              string `json:"code"`
+	ChatID            int64  `json:"chat_id"`
+	UserTelegramLogin string `json:"user_telegram_login"`
+	UserTelegramID    int64  `json:"user_telegram_id"`
+}
