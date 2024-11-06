d5fe8d6cee97111b7c1116c70cd97e80ab5d6df3
Pavel Tyukin <me@patyukin.ru>
2024-11-06 21:41:06 +0300

    update consumer

--- pkg/rabbitmq/consumer.go
+++ pkg/rabbitmq/consumer.go
@@ -5,14 +5,11 @@ import (
 	"fmt"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/rs/zerolog/log"
-	"sync"
 )
 
-// AuthNotifyConsumer - consumer for auth_notify_queue
-// Обработка сообщений из очереди auth_notify_queue
-func (r *RabbitMQ) AuthNotifyConsumer(ctx context.Context, processMessage HandlerFunction, workerCount int) error {
+func (r *RabbitMQ) Consume(ctx context.Context, queue string, processMessage HandlerFunction) error {
 	msgs, err := r.channel.Consume(
-		AuthNotifyQueue,
+		queue,
 		"",
 		false,
 		false,
@@ -22,62 +19,6 @@ func (r *RabbitMQ) AuthNotifyConsumer(ctx context.Context, processMessage Handle
 	)
 	if err != nil {
 		return fmt.Errorf("failed to consume in auth_notify_queue: %w", err)
-	}
-
-	wg := &sync.WaitGroup{}
-	wg.Add(workerCount)
-
-	// run workers
-	for i := 0; i < workerCount; i++ {
-		go func(workerID int) {
-			defer wg.Done()
-			for {
-				select {
-				case <-ctx.Done():
-					log.Error().Msgf("worker %d: context is done", workerID)
-					return
-				case msg, ok := <-msgs:
-					if !ok {
-						log.Error().Msgf("worker %d: channel is closed", workerID)
-						return
-					}
-
-					if err = processMessage(ctx, msg); err != nil {
-						if nackErr := msg.Nack(false, true); nackErr != nil {
-							log.Printf("worker %d: failed to nack message: %v", workerID, nackErr)
-							continue
-						}
-
-						log.Printf("worker %d: failed to process message: %v", workerID, err)
-						continue
-					}
-
-					if ackErr := msg.Ack(false); ackErr != nil {
-						log.Printf("worker %d: failed to ack message: %v", workerID, ackErr)
-					}
-				}
-			}
-		}(i + 1)
-	}
-
-	wg.Wait()
-	log.Info().Msg("All workers finished")
-
-	return nil
-}
-
-func (r *RabbitMQ) NotifyAuthConsumer(ctx context.Context, processMessage HandlerFunction) error {
-	msgs, err := r.channel.Consume(
-		NotifyAuthQueue,
-		"",
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		return fmt.Errorf("failed to consume in auth_sign_up_confirm_code_queue: %w", err)
 	}
 
 	for msg := range msgs {
