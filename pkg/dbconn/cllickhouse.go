46efeefdd968e63c46f7ba0574626d241cdfd23f
Pavel Tyukin <me@patyukin.ru>
2024-11-12 23:09:12 +0300

    update clickhouse

---
+++ pkg/dbconn/cllickhouse.go
@@ -0,0 +1,30 @@
+package dbconn
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/rs/zerolog/log"
+	"time"
+)
+
+func NewClickhouse(ctx context.Context, dsn string) (*sql.DB, error) {
+	dbConn, err := sql.Open("clickhouse", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to db: %w", err)
+	}
+
+	dbConn.SetConnMaxLifetime(time.Hour)
+
+	log.Info().Msg("connected to db")
+
+	err = dbConn.PingContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping db: %w", err)
+	}
+
+	log.Info().Msg("pinged clickhouse db")
+
+	return dbConn, nil
+}
