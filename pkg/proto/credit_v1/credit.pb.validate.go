d0416b288d8352489b5dbbd0a7916e1bdc344bca
Pavel Tyukin <me@patyukin.ru>
2024-12-02 19:11:50 +0300

    update proto

--- pkg/proto/credit_v1/credit.pb.validate.go
+++ pkg/proto/credit_v1/credit.pb.validate.go
@@ -422,6 +422,35 @@ func (m *CreditApplicationConfirmationResponse) validate(all bool) error {
 
 	// no validation rules for Message
 
+	if all {
+		switch v := interface{}(m.GetError()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, CreditApplicationConfirmationResponseValidationError{
+					field:  "Error",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, CreditApplicationConfirmationResponseValidationError{
+					field:  "Error",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetError()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return CreditApplicationConfirmationResponseValidationError{
+				field:  "Error",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return CreditApplicationConfirmationResponseMultiError(errors)
 	}
